71d23744590520b5894d52fcaf22858cf88a1507
abraham-ship <bettabraham11@gmail.com>
2023-09-25 08:58:49 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,7 +4,7 @@ a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 '''
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import requests
     from sys import argv
 
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     total_tasks = len(todo_list)
     completed_tasks = sum(1 for task in todo_list if task["completed"])
 
-    print(f"Employee {employee_name} is done with tasks\
-            ({completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/\
+{total_tasks}):")
 
     for task in todo_list:
         if task["completed"]:
