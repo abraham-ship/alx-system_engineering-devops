6f8c06f8c20283abe3a9c8fe9964d86825dd5b55
abraham-ship <bettabraham11@gmail.com>
2023-09-25 08:12:23 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     from sys import argv
 
     user_id = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/\
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/\
             {user_id}").json()
     employee = user.get("username")
-    tasks = requests.get("https://jsonplaceholder.typicode.com/todos?userId=\
+    tasks = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId=\
             {user_id}").json()
 
     with open("{user_id}.csv", mode='w') as f:
